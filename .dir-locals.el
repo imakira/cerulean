922748871738221c99a3af62b5474edbced588e3
Akira <coruscationmail@gmail.com>
2026-01-25 07:31:40 +0800

    editor: set default cider testing exclude selectors in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ((nil . ((cider-clojure-cli-global-options . "-A:dev:uberjar")
          (cider-preferred-build-tool       . clojure-cli)
          (cider-default-cljs-repl          . custom)
+         (cider-test-default-exclude-selectors . ("integration" "flakey" "slow"))
          (cider-custom-cljs-repl-init-form . "(do (require 'net.coruscation.cerulean.shadow-dev) (net.coruscation.cerulean.shadow-dev/cljs-repl))")
          (eval . (progn
                    (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
